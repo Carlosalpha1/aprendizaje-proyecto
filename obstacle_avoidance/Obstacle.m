21a29b729975ba11345ee3bec9ef7d7e49f23b6d
Carlos Caminero <carlos2caminero@gmail.com>
2021-11-29 13:29:02 +0100

    update obstacle class

--- obstacle_avoidance/Obstacle.m
+++ obstacle_avoidance/Obstacle.m
@@ -1,24 +1,45 @@
-classdef Obstacle
-    properties
+% -----------------------------------------------
+% -- from Obstacle Avoidance
+% --    Authors:    Javier Martinez
+% --                Irene Bandera
+% --                Veronica Tornero
+% --                Carlos Caminero
+% --
+% -- class Obstacle:
+% --    * Obstacle(x, y) -> Constructor
+% --    * show() -> None
+% -- It represents an Obstacle on Environment.
+% -----------------------------------------------
+
+classdef Obstacle < handle
+    
+    properties (Access=public)
+        x
+        y
+        side = 0.75
+    end
+    
+    properties (Access=private)
         x1
         x2
         y1
         y2
-        side=1
     end
     
     methods
         function self = Obstacle(x, y)
+            self.x = x;
+            self.y = y;
             self.x1 = x-self.side/2;
             self.y1 = y-self.side/2;
             self.x2 = x+self.side/2;
-            self.y2 = x+self.side/2;
+            self.y2 = y+self.side/2;
         end
         
         function show(self)
-            x = [self.x1, self.x2, self.x2, self.x1, self.x1];
-            y = [self.y1, self.y1, self.y2, self.y2, self.y1];
-            plot(x, y, '-b', 'LineWidth', 3);
+            X = [self.x1, self.x2, self.x2, self.x1, self.x1];
+            Y = [self.y1, self.y1, self.y2, self.y2, self.y1];
+            plot(X, Y, '-b', 'LineWidth', 2);
             hold on;
         end
     end
