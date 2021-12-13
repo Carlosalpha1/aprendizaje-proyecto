6376c6b04b876010077abde42dc0ac02fbb06437
Veronica274 <v.tornero.27@gmail.com>
2021-12-13 12:10:42 +0100

    visualizacion del robot con obstaculo y trayectoria

--- obstacle_avoidance/Environment.m
+++ obstacle_avoidance/Environment.m
@@ -16,14 +16,13 @@ classdef Environment < handle
     properties
         robot
         obstacles = []
-        road
        
 
     end
     methods
         function self=Environment()
-            self.road = plot([-7 -7],[0 10], [2 2], [0 10]);
-            axis([-12 6 0 10]);
+            plot([-7 -7],[0 10], [2 2], [0 10]);
+            axis([-10 10 0 20]);
             hold on
 
         end
@@ -37,11 +36,15 @@ classdef Environment < handle
         end
 
         function show(self)
+            clf;
+            plot([-7 -7],[0 10], [2 2], [0 10]);
+            hold on
             for i=1:length(self.obstacles)
                 self.obstacles(i).show()
             end
-
             self.robot.show()
+            axis([-10 10 0 20]);
+            pause(0.01)
         end
 
     end
--- obstacle_avoidance/main.m
+++ obstacle_avoidance/main.m
@@ -1,13 +1,16 @@
 clc; clear all; close all;
 
 environment = Environment();
-
+j = 0.5;
 obst = Obstacle(1, 2);
 
-robot = Robot(-2, 0.5);
+robot = Robot(-2.5, 0.5);
 
 environment.addObstacle(obst);
 
 environment.addRobot(robot);
 
-environment.show()
+for i=1:20
+    environment.robot.move(0.3, 0)
+    environment.show()
+end
