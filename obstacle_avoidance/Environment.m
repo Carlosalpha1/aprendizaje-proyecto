2794f816bd33b216f0bdf12fb0340d10fb97a93f
jmrtzma <javimartinezmma@gmail.com>
2021-12-16 20:47:38 +0100

    fixing environment show

--- obstacle_avoidance/Environment.m
+++ obstacle_avoidance/Environment.m
@@ -34,6 +34,7 @@ classdef Environment < handle
             clf;
             plot([-4 -4],[0 20], [4 4], [0 20]);
             hold on
+            axis([-10 10 0 10])
             
             for i=1:length(self.obstacles)
                 self.obstacles(i).show()
