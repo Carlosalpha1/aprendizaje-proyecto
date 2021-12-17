0669c9f6e03f8052672f4cec3392d1b637ed6043
Carlos Caminero <carlos2caminero@gmail.com>
2021-12-17 12:15:05 +0100

    neural network (bugs)

--- obstacle_avoidance/gendataset.m
+++ obstacle_avoidance/gendataset.m
@@ -10,34 +10,49 @@
 
 clear all; close all; clc;
 
-NSIM = 30000;  % -- simulations
+NSIM = 3000;  % -- simulations
+FACTOR = 0.008;
 
-laser_values = zeros([1, 180]);
-
+% -- main ----------------------
 data = [];
+laser_values = zeros([1, 90]);
+
+environment = Environment();
+
+obst1 = Obstacle(1, 5);
+obst2 = Obstacle(-3, 7);
+obst3 = Obstacle(-2, 2);
+environment.addObstacle(obst1);
+environment.addObstacle(obst2);
+environment.addObstacle(obst3);
+
+laser = Laser(180, 2);
 
 for i=1:NSIM
-    % -- read laser lecture simulation
-    for j=1:length(laser_values)
-        laser_values(j) = (-1 + 21)*rand();
-    end
+    %clf;
+    robot = Robot(-5 + 10*rand(), 5*rand());
+    environment.addRobot(robot)
+    laser.update(environment)
+    laser_values = laser.get_values();
     
-    [lm, rm, vx, az] = velocity_from_laser(laser_values);
-    data = [data; lm, rm, vx, az];
+    [lm, rm, az] = velocity_from_laser(laser_values, FACTOR);
+    data = [data; lm, rm, az];
 end
-
 save('dataset.mat', 'data');
 
-function [lm, rm, vx, az] = velocity_from_laser(values)
-    factor = 0.8;
+% --------------------------------
+
+
+
+function [lm, rm, az] = velocity_from_laser(values, factor)
     n = length(values);
-    left_mean = mean_(values(1, 1:n/2));
-    right_mean = mean_(values(1, (n/2+1):n));
-    
-    vx = 0.3;
-    az = factor / (-left_mean + right_mean);
-    lm = left_mean;
-    rm = right_mean;
+    lm = mean_(values(1, 1:n/2));
+    rm = mean_(values(1, (n/2+1):n));
+    if (lm == rm)
+        az = 0;
+    else
+        az = factor / (-lm + rm);
+    end
 end
 
 function m = mean_(arr)
--- obstacle_avoidance/main.m
+++ obstacle_avoidance/main.m
@@ -1,10 +1,11 @@
 clc; clear all; close all;
+load dataset.mat
 
 environment = Environment();
 
-obst1 = Obstacle(2, 5);
-obst2 = Obstacle(-4, 7);
-obst3 = Obstacle(-3, 2);
+obst1 = Obstacle(1, 5);
+obst2 = Obstacle(-3, 7);
+obst3 = Obstacle(-2, 2);
 environment.addObstacle(obst1);
 environment.addObstacle(obst2);
 environment.addObstacle(obst3);
@@ -14,14 +15,44 @@ environment.addRobot(robot)
 
 laser = Laser(180, 2);
 
-laser.update(environment)
+% -- Train Neural Network ---------
+X = data(:, 1:2)'
+Y = data(:, 3)'
+
+net = fitnet(1, 'trainlm');
+net.trainParam.epochs = 10000;
+net.trainParam.goal = 0.000000000000001
+net.trainParam.max_fail = 100;
+net.divideParam.trainRatio = 70/100;
+net.divideParam.valRatio = 15/100;
+net.divideParam.testRatio = 15/100;
+net = train(net, X, Y);
+% ----------------------------------
 
 for i=1:100
     clf;
-    environment.robot.move(0.1, 0)
     laser.update(environment)
-    environment.show()
-    laser.show()
     v = laser.get_values();
-    pause(0.001);
+    left_mean = mean_(v(1, 1:length(v)/2));
+    right_mean = mean_(v(1, (length(v)/2+1):length(v)));
+    az = sim(net, [left_mean; right_mean]);
+    % fprintf("az: %f\n", az);
+    environment.robot.move(0.1, az)
+    environment.show();
+    laser.show();
+    pause(0.01);
+end
+
+
+function m = mean_(arr)
+    [~, n] = size(arr);
+    m = 0;
+    c = 0;
+    for i=1:n
+        if arr(i) > 0
+            m = m + arr(i);
+            c = c + 1;
+        end
+    end
+    m = m/c;
 end
